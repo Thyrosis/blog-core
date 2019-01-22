1151e327c544fa97af5c2c4998fd0cfb65dacfcf
Maarten @ BTO <10061757+Thyrosis@users.noreply.github.com>
2019-01-22 22:24:45 +0100

    Create post-type migration
    Add the post-type migration file in preparation of setting the form type per post.
    
    Also updated previous migration files to use the dropColum on down-methods.

--- database/migrations/2018_09_22_211838_add_description_to_category.php
+++ database/migrations/2018_09_22_211838_add_description_to_category.php
@@ -26,7 +26,7 @@ class AddDescriptionToCategory extends Migration
     public function down()
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->dropColumn('description');
         });
     }
 }
--- database/migrations/2019_01_22_222126_add_posttype_to_posts_table.php
+++ database/migrations/2019_01_22_222126_add_posttype_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTrackingToComments extends Migration
+class AddPosttypeToPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddTrackingToComments extends Migration
      */
     public function up()
     {
-        Schema::table('comments', function (Blueprint $table) {
-            $table->boolean('notify')->default(0);
+        Schema::table('posts', function (Blueprint $table) {
+            $table->text('type')->default('post');
         });
     }
 
@@ -25,8 +25,8 @@ class AddTrackingToComments extends Migration
      */
     public function down()
     {
-        Schema::table('comments', function (Blueprint $table) {
-            $table->dropColumn('notify');
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('type');
         });
     }
 }
