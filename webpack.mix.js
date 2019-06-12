c5b951aa74b17a91f2317eef1904e39b3874f640
Maarten @ BTO <10061757+Thyrosis@users.noreply.github.com>
2019-06-12 13:16:39 +0200

    Run NPM production
    Run NPM production and save the results for better speed.
    
    Remove dependencies:
    - Axios
    - Lodash
    
    Update dependencies:
    - Laravel Mix to 4.0.16
    - TailwindCSS from 1.0.1 to 1.0.4
    - Sass from 1.16.1 to 1.21

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 require('laravel-mix-tailwind');
 
@@ -13,6 +13,6 @@ require('laravel-mix-tailwind');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js/core.js')
-   .sass('resources/assets/sass/app.scss', 'public/css/core.css')
+mix.js('resources/js/app.js', 'public/js/core.js')
+   .sass('resources/sass/app.scss', 'public/css/core.css')
    .tailwind('./tailwind.config.js');
